5b36ec000930ea15d56025043a3f938206d948ee
sugyan <sugi1982@gmail.com>
2025-04-25 23:16:38 +0900

    Add update_schedule.py

---
+++ scripts/update_schedule.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from random import choices, randint
+
+import yaml
+
+
+def main() -> None:
+    schedule = generate_schedule()
+    update_schedule(sorted(schedule))
+
+
+def generate_schedule() -> list[tuple[int, int]]:
+    num = choices([2, 3, 4, 5], weights=[2, 5, 5, 1])[0]
+    return [(randint(8, 23), randint(0, 59)) for _ in range(num)]
+
+
+def update_schedule(schedule: list[tuple[int, int]]) -> None:
+    path = Path(__file__).parent.parent / ".github" / "workflows" / "post.yml"
+    with path.open("r") as f:
+        doc = yaml.safe_load(f)
+    doc["on"]["schedule"] = [
+        {"cron": f"{minute} {(hour + 15) % 24} * * *"} for hour, minute in schedule
+    ]
+    with path.open("w") as f:
+        yaml.dump(doc, f, sort_keys=False, indent=2)
+
+
+if __name__ == "__main__":
+    main()
